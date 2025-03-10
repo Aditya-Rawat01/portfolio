d4afb3d11f5f70ecf03385b19ab25b8ba908845a
Aditya Rawat <adityarawat240@gmail.com>
2025-03-11 00:28:00 +0530

    initial

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,19 @@
+import { GithubIcon } from "../icons/githubIcon";
+import { LinkedinIcon } from "../icons/linkedinIcon";
+import { TwitterIcon } from "../icons/twitterIcon";
 import { Sidebar } from "./sidebar";
 
 export function Header() {
     return (
-        <div className="w-[99%] h-20 bg-white shadow-[inset_0px_-18px_24px_-15px_rgba(249,_115,_22,_0.5)] backdrop-blur-lg fixed top-4 text-black rounded-full flex items-center justify-between">
+        <div className="w-[99%] h-16 bg-white shadow-[inset_0px_-18px_24px_-15px_rgba(249,_115,_22,_0.5)] backdrop-blur-lg fixed top-4 text-black rounded-full flex items-center justify-between">
             <Sidebar/>
             <p className="text-2xl">portfolio</p>
-            <p>//github/twitter</p>
+            <div className=" gap-3 mr-4 hidden sm:flex">
+            <p className="cursor-pointer"><GithubIcon/></p>
+            <p className="cursor-pointer"><TwitterIcon/></p>
+            <p className="cursor-pointer"><LinkedinIcon/></p>    
+            </div>
+            
 
         </div>
     )
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -5,12 +5,15 @@ import { CancelIcon } from "../icons/cancelIcon";
 
 export function Sidebar() {
     const [open,setOpen]=useState(false)
-    console.log(open)
+    
     return (
-        <div className="">
-            <p className="cursor-pointer" onClick={()=>setOpen((prev)=>!prev)}>
+        <div className="relative">
+            <p className="cursor-pointer relative z-10 sm:hidden" onClick={()=>setOpen((prev)=>!prev)}>
                 {!open?<SidebarIcon/>:<CancelIcon/>}
             </p>
+            <div className={`fixed ${open?"left-0":"-left-[110%]"} transition-all duration-1000 top-0 w-full bg-gray-500 h-screen rounded-2xl`}>
+
+            </div>
         </div>
     )
 }
